5820c373d9b712624def806882bb3c7e82ef2cfa
Ha Phan <44280407+habmc@users.noreply.github.com>
2019-09-20 23:00:12 -0400

    Add js file and css file

---
+++ src/script.js
@@ -0,0 +1,49 @@
+const app = document.getElementById('root');
+
+var name;
+function check() {
+    if (document.getElementById('root') !== null) {
+        document.getElementById('root').innerHTML = "";
+    }
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    app.appendChild(container);
+    name = document.getElementById('searchTxt').value;
+    console.log(name);
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + name;
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (resp) {
+            if (resp && resp.drinks) {
+                resp.drinks.forEach(drink => {
+                    const card = document.createElement('div');
+                    card.setAttribute('class', 'card');
+
+                    const h1 = document.createElement('h1');
+                    h1.textContent = drink.strDrink;
+
+                    const image = document.createElement("img");
+                    image.src = drink.strDrinkThumb;
+                    image.id = "id";
+                    image.className = "class";
+                    image.src = drink.strDrinkThumb;
+                    card.appendChild(image);
+
+                    const p = document.createElement('p');
+                    p.textContent = "Instruction: " + `${drink.strInstructions}`;
+
+                    container.appendChild(card);
+                    card.appendChild(h1);
+                    card.appendChild(p);
+                });
+            }
+            else {
+                const p = document.createElement('p');
+                p.textContent = "Sorry we don't have a recipe for that drink :(";                
+                container.appendChild(p);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
